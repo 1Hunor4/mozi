27a598d16d17d83ecab4dcacee4cbd3eeffac5f7
1Hunor4 <c2verhun@vasvari.org>
2025-01-27 13:16:05 +0100

    01.27 orak vegi

--- projekt.py
+++ projekt.py
@@ -1,1 +1,9 @@
-szekek = []
+import random
+arak = [2500, 1500, 1300, "ÜRES"]
+def nezoter():
+    for i in range(15):
+        ulesek = []
+        for j in range(20):
+            ulesek.append(random.choice(arak))
+    print(ulesek)
+nezoter()
